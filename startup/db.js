469cb7edbd718afb0d4fdff6ccac9079354f5494
Mohd Tamsil <tamsilansari29@gmail.com>
2022-09-16 00:40:22 +0530

    fixed a db bug

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require('config');
 
 module.exports = function () {
+  const db = config.get('db');
   mongoose
-    .connect("mongodb://localhost/vidly")
+    .connect(db)
     .then(() => winston.info("Connected to MongoDB..."));
 };
